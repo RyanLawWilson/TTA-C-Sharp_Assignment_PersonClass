8dc10323f8c977be2ca7b8b1d9f168a8aebcceae
RyanLawWilson <59398576+RyanLawWilson@users.noreply.github.com>
2020-03-02 16:13:17 -0700

    Add project files.

---
+++ PersonClass/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonClass
+{
+    public class Employee : Person
+    {
+        public int id { get; set; }
+    }
+}
